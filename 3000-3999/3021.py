c2484e543f521b4c8da9544adfd819f47d91da1d
CJ Wang <lejacp@gmail.com>
2025-08-29 10:45:21 +0800

    add solution and explanation for 3021. Alice and Bob Playing Flower Game

---
+++ 3000-3999/3021.py
@@ -0,0 +1,7 @@
+class Solution:
+    def flowerGame(self, n: int, m: int) -> int:
+        odd_n = n // 2 + n % 2
+        even_n = n // 2
+        odd_m = m // 2 + m % 2
+        even_m = m // 2
+        return odd_n * even_m + even_n * odd_m
